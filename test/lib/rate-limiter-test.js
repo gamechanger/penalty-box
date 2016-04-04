7c4943a37cffb548f41625e582a2ff2fb5e2683e
paetling <paetling@gmail.com>
2016-04-04 14:19:46 -0400

    add in more tests

--- test/lib/rate-limiter-test.js
+++ test/lib/rate-limiter-test.js
@@ -6,36 +6,55 @@ var rateLimiter = require('../../lib/rate-limiter');
 describe('Test the functions that are adding values to redis', function() {
     describe('Test ensureAppAndKey', function () {
         it('Test basic setup', function(){
-            testClient.innerDict = {}
+            testClient.innerDict = {};
             rateLimiter.ensureAppAndKey("testApp", "key1", 60, testClient, function(){})
             assert.equal('60', testClient.innerDict['testApp:key1_rl']);
             assert.equal('60', testClient.innerDict['testApp:key1_tkns']);
             assert.equal('testApp:key1_epoch_ms' in testClient.innerDict, true);
-            expectedList = ["testApp", "key1"]
-            assert.equal(expectedList.length, testClient.innerDict['testApp'].length);
-            for (i=0; i < expectedList.length; i ++){
-                assert.equal(expectedList[i], testClient.innerDict['testApp'][i])
-            }
+        });
+        it('Test more complicated setup', function(){
+            testClient.innerDict = {};
+            rateLimiter.ensureAppAndKey("testApp", "key1", 60, testClient, function(){})
+            rateLimiter.ensureAppAndKey("testApp", "key2", 55, testClient, function(){})
+            rateLimiter.ensureAppAndKey("testApp", "key3", 50, testClient, function(){})
+            rateLimiter.ensureAppAndKey("testApp", "key1", 45, testClient, function(){})
+            assert.equal(9, _.keys(testClient.innerDict).length)
+            assert.equal('60', testClient.innerDict['testApp:key1_rl']);
+            assert.equal('60', testClient.innerDict['testApp:key1_tkns']);
+            assert.equal('testApp:key1_epoch_ms' in testClient.innerDict, true);
+            assert.equal('55', testClient.innerDict['testApp:key2_rl']);
+            assert.equal('55', testClient.innerDict['testApp:key2_tkns']);
+            assert.equal('testApp:key2_epoch_ms' in testClient.innerDict, true);
+            assert.equal('50', testClient.innerDict['testApp:key3_rl']);
+            assert.equal('50', testClient.innerDict['testApp:key3_tkns']);
+            assert.equal('testApp:key3_epoch_ms' in testClient.innerDict, true);
         });
     });
-    describe('Test appKeysTokens', function(){
+    describe('Test keyToken', function(){
         it('Test Basic Setup', function(){
-            testClient.innerDict = {}
+            testClient.innerDict = {};
             testClient.innerDict["testApp"] = ["key1", "key2", "key3"];
-            testClient.innerDict["testApp:key1_tkns"] = "60"
-            testClient.innerDict["testApp:key2_tkns"] = "55"
-            testClient.innerDict["testApp:key3_tkns"] = "50"
-            rateLimiter.appKeysTokens("testApp", testClient, function(err, response){
+            testClient.innerDict["testApp:key1_tkns"] = "60";
+            testClient.innerDict["testApp:key2_tkns"] = "55";
+            testClient.innerDict["testApp:key3_tkns"] = "50";
+            rateLimiter.keyToken("testApp", "key1", testClient, function(err, response){
                 assert.equal(null, err);
-                assert.equal(true, _.isEqual({"key1": "60", "key2": "55", "key3": "50"}, response));
+                assert.equal(60, response);
+            });
+            rateLimiter.keyToken("testApp", "key2", testClient, function(err, response){
+                assert.equal(null, err);
+                assert.equal(55, response);
+            });
+            rateLimiter.keyToken("testApp", "key3", testClient, function(err, response){
+                assert.equal(null, err);
+                assert.equal(50, response);
             });
         });
         it('Empty dict', function(){
-            testClient.innerDict = {"testApp": []}
-            rateLimiter.appKeysTokens("testApp", testClient, function(err, response){
+            testClient.innerDict = {};
+            rateLimiter.keyToken("testApp", "key1", testClient, function(err, response){
                 assert.equal(null, err);
-                console.log(response)
-                assert.equal(true, _.isEqual({}, response));
+                assert.equal(null, response);
             });
         });
     });
