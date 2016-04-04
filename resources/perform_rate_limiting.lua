9ada7d5f0d20f7a502af1af7c5902e7ca7a42dfc
paetling <paetling@gmail.com>
2016-04-04 14:02:35 -0400

    add in rate lua rate limiting script

---
+++ resources/perform_rate_limiting.lua
@@ -0,0 +1,25 @@
+local rlKey = KEYS[1]
+local tokenKey = KEYS[2]
+local epochKey = KEYS[3]
+
+local currentEpoch = tonumber(ARGV[1])
+local requestCost = tonumber(ARGV[2])
+
+local rateLimitPerHour = tonumber(redis.call('get', rlKey))
+local currentTokens = tonumber(redis.call('get', tokenKey))
+local pastEpoch = tonumber(redis.call('get', epochKey))
+local epochDiffSec = (currentEpoch - pastEpoch) / 1000
+
+if (epochDiffSec > 180) then
+    addTokens = epochDiffSec * rateLimitPerHour / 3600
+    currentTokens = math.min(currentTokens + addTokens, rateLimitPerHour)
+    redis.call('set', tokenKey, tostring(currentTokens))
+end
+
+if (currentTokens <= 0) then
+    return -1
+else
+    currentTokens = math.max(mincurrentTokens - requestCost, 0)
+    redis.call('set', tokenKey, tostring(currentTokens))
+    return currentTokens
+end
