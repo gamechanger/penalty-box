84e761c06686a02572e09b73fe27acdc7b4167a9
paetling <paetling@gmail.com>
2016-04-19 17:05:38 -0400

    send tags properly

--- middleware/metrics.js
+++ middleware/metrics.js
@@ -7,16 +7,16 @@ statsd = new StatsD(config.datadog_host, config.datadog_port);
 prefix = "penalty-box."
 
 exports.handle = function(req, res, next){
-    tags = {'request_path': req.path};
+    tags = ['request_path:'+ req.path];
     statsd.increment(prefix + 'requests', 1, tags);
     statsd.gauge(prefix + 'open_sockets', _(http.globalAgent.sockets).reduce(function(m, arr) {return m + arr.length;}, 0));
 
     var start = Date.now();
     res.on('finish', function() {
-        var tags = {
-            http_code:  res.statusCode,
-            request_path: req.path
-        };
+        var tags = [
+            'http_code:'+res.statusCode,
+            'request_path:'+req.path
+        ];
         statsd.increment(prefix + 'responses', 1, tags);
         statsd.timing(prefix + 'response_time', Date.now()-start, tags)
     });
