113bbb502d473deb6995edc816aa7a740434982e
paetling <paetling@gmail.com>
2016-04-19 16:53:56 -0400

    add in penalty-box prefix to metrics

--- middleware/metrics.js
+++ middleware/metrics.js
@@ -4,11 +4,12 @@ var StatsD = require('node-dogstatsd').StatsD;
 var config = require('../lib/config');
 
 statsd = new StatsD(config.datadog_host, config.datadog_port);
+prefix = "penalty-box."
 
 exports.handle = function(req, res, next){
     tags = {'request_path': req.path};
-    statsd.increment('requests', 1, tags);
-    statsd.gauge('open_sockets', _(http.globalAgent.sockets).reduce(function(m, arr) {return m + arr.length;}, 0));
+    statsd.increment(prefix + 'requests', 1, tags);
+    statsd.gauge(prefix + 'open_sockets', _(http.globalAgent.sockets).reduce(function(m, arr) {return m + arr.length;}, 0));
 
     var start = Date.now();
     res.on('finish', function() {
@@ -16,8 +17,8 @@ exports.handle = function(req, res, next){
             http_code:  res.statusCode,
             request_path: req.path
         };
-        statsd.increment('responses', 1, tags);
-        statsd.timing('response_time', Date.now()-start, tags)
+        statsd.increment(prefix + 'responses', 1, tags);
+        statsd.timing(prefix + 'response_time', Date.now()-start, tags)
     });
 
     next();
