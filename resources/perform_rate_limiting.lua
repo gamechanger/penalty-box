74f3a0f5830ea025439cb272617ec720936be36e
paetling <paetling@gmail.com>
2016-04-28 12:01:57 -0400

    change strings to ints

--- resources/perform_rate_limiting.lua
+++ resources/perform_rate_limiting.lua
@@ -1,23 +1,26 @@
 local rlKey = KEYS[1]
 local tokenKey = KEYS[2]
 local epochKey = KEYS[3]
-local currentEpoch = ARGV[1]
-local requestCost = ARGV[2]
-local rateLimit = ARGV[3]
+local currentEpoch = tonumber(ARGV[1])
+local requestCost = tonumber(ARGV[2])
+local rateLimit = tonumber(ARGV[3])
 local keyTimeout = ARGV[4]
 
 local secondsInMinute = 60
 local millisecondsInSecond = 1000
+local returnStatus = 0
 
 local storedRateLimit = redis.call('get', rlKey)
 if storedRateLimit == false then
     storedRateLimit = rateLimit
     redis.call('set', rlKey, storedRateLimit, 'EX', keyTimeout)
+else
+    storedRateLimit = tonumber(storedRateLimit)
 end
 
 local storedEpoch = redis.call('get', epochKey)
 local currentTokens = 0
-if storedEpoch == false or (currentEpoch - storedEpoch) > (secondsInMinute * millisecondsInSecond) then
+if storedEpoch == false or (currentEpoch - tonumber(storedEpoch)) > (secondsInMinute * millisecondsInSecond) then
     redis.call('set', epochKey, currentEpoch, 'EX', keyTimeout)
     currentTokens = storedRateLimit
 else
@@ -25,6 +28,8 @@ else
     if currentTokens == false then
         redis.call('set', tokenKey, storedRateLimit, 'EX', keyTimeout)
         currentTokens = storedRateLimit
+    else
+        currentTokens = tonumber(currentTokens)
     end
 end
 
@@ -33,7 +38,6 @@ if ((currentTokens <= 0) or (requestCost > currentTokens)) then
 else
     currentTokens = math.max(currentTokens - requestCost, 0)
     redis.call('set', tokenKey, currentTokens, 'EX', keyTimeout)
-    returnStatus = 0
 end
 
 return {returnStatus, currentTokens}
