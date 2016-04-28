86e0321791972d506cb7162efcdf760b4ebca5f8
paetling <paetling@gmail.com>
2016-04-28 11:54:48 -0400

    redesign lua script for maximum efficiency

--- resources/perform_rate_limiting.lua
+++ resources/perform_rate_limiting.lua
@@ -1,34 +1,38 @@
 local rlKey = KEYS[1]
 local tokenKey = KEYS[2]
 local epochKey = KEYS[3]
-local currentEpoch = tonumber(ARGV[1])
-local requestCost = tonumber(ARGV[2])
+local currentEpoch = ARGV[1]
+local requestCost = ARGV[2]
 local rateLimit = ARGV[3]
 local keyTimeout = ARGV[4]
 
--- Ensure that the rate limit keys are set up
-redis.call('set', rlKey, rateLimit, 'EX', keyTimeout, 'NX')
-redis.call('set', epochKey, currentEpoch, 'EX', keyTimeout, 'NX')
-redis.call('setnx', tokenKey, rateLimit)
+local secondsInMinute = 60
+local millisecondsInSecond = 1000
 
--- Perform the actual rate limit logic
-local returnStatus
-local rateLimitPerMinute = tonumber(redis.call('get', rlKey))
-local pastEpoch = tonumber(redis.call('get', epochKey))
-local epochDiffSec = (currentEpoch - pastEpoch) / 1000
-
-if (epochDiffSec > 60) then
-    redis.call('setex', tokenKey, keyTimeout, tostring(rateLimitPerMinute))
-    redis.call('setex', epochKey, keyTimeout, tostring(currentEpoch))
+local storedRateLimit = redis.call('get', rlKey)
+if storedRateLimit == false then
+    storedRateLimit = rateLimit
+    redis.call('set', rlKey, storedRateLimit, 'EX', keyTimeout)
 end
 
-local currentTokens = tonumber(redis.call('get', tokenKey))
+local storedEpoch = redis.call('get', epochKey)
+local currentTokens = 0
+if storedEpoch == false or (currentEpoch - storedEpoch) > (secondsInMinute * millisecondsInSecond) then
+    redis.call('set', epochKey, currentEpoch, 'EX', keyTimeout)
+    currentTokens = storedRateLimit
+else
+    currentTokens = redis.call('get', tokenKey)
+    if currentTokens == false then
+        redis.call('set', tokenKey, storedRateLimit, 'EX', keyTimeout)
+        currentTokens = storedRateLimit
+    end
+end
 
 if ((currentTokens <= 0) or (requestCost > currentTokens)) then
     returnStatus = -1
 else
     currentTokens = math.max(currentTokens - requestCost, 0)
-    redis.call('setex', tokenKey, keyTimeout, tostring(currentTokens))
+    redis.call('set', tokenKey, currentTokens, 'EX', keyTimeout)
     returnStatus = 0
 end
 
